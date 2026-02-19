cedf9772825f4ba42bfba389265b41e97f052d07
judy0907p <jiayizhai0907@gmail.com>
2026-02-18 18:32:47 -0800

    project update

---
+++ DAY18/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+#if use import turtle, then need to type turtle.Turtle() all the time
+# import turtle
+#tim=turtle.Turtle()
+
+
+#from turtle import * will import all the functions from turtle module
+#import turtle as t will import the turtle module and give it an alias t
+
+
+timmy_the_turtle=Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("red")
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+
+#Chanllenge1-draw a square
+# for _ in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
