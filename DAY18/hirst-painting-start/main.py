5f43f8d503c6ffef0c49c320f1f103201c6a91e5
judy0907p <jiayizhai0907@gmail.com>
2026-02-19 16:41:09 -0800

    day18 update

---
+++ DAY18/hirst-painting-start/main.py
@@ -0,0 +1,46 @@
+###This code will not work in repl.it as there is no access to the colorgram package here.###
+##We talk about this in the video tutorials##
+import colorgram
+
+#方案 C（最稳、最专业）：路径永远以脚本所在目录为准（强烈推荐）
+#不管你从哪运行，都能找到同级图片：
+from pathlib import Path
+import colorgram
+import random
+
+# HERE = Path(__file__).resolve().parent
+# img_path = HERE / "image.jpg"
+# rgb_colors = []
+# colors = colorgram.extract(str(img_path), 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+# print(rgb_colors)
+
+import turtle as t
+t.colormode(255)
+tim = t.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+color_list=[ (202, 164, 110), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+num_of_dots = 100
+for dot_count in range(1,num_of_dots+1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen = t.Screen()
+screen.exitonclick()    
